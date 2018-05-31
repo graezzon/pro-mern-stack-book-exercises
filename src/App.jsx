3fcc6f9f584d799fab5c12ab606a130728c5456f
graezzon <graezzon@gmail.com>
2018-05-30 20:15:31 -0700

    Added more work to beg of chap 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,72 @@
 const contentNode = document.getElementById('contents');
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ').split("\n");
-const component = <p>{message}</p>;      // A simple JSX component
-ReactDOM.render(component, contentNode);  // Render the component inside the content Node
+
+class IssueFilter extends React.Component{
+    render(){
+        return(
+            <div>IssueFilter</div>
+        )
+    }
+}
+
+class IssueTable extends React.Component{
+    render(){
+        const borderedStyle = {border: "1px solid silver", background: "#eee", padding:10}
+        return(
+            <table style={{borderedCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style={borderedStyle}>Id</th>
+                        <th style={borderedStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow issue_id={1}
+                    issue_title="Error in Console when clicking Add" />
+                    <IssueRow issue_id={2}
+                    issue_title="Missing Panel on bottom" />
+                    <IssueRow issue_id={3}
+                    issue_title="Misaligned columns" />                     
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class IssueRow extends React.Component {
+    render() {
+        const borderedStyle = {border: "1px solid silver", padding: 8};
+        return(            
+            <tr>
+                <td style={borderedStyle}>{this.props.issue_id}</td>
+                <td style={borderedStyle}>{this.props.issue_title}</td>
+            </tr>
+        )
+    }
+}
+
+class IssueAdd extends React.Component{
+    render(){
+        return(
+            <div>IssueAdd</div>
+        )
+    }
+}
+
+
+class IssueList extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>IssueTracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<IssueList />, contentNode);  // Render the component inside the content Node
--- static/App.js
+++ static/App.js
@@ -1,13 +1,175 @@
-'use strict';
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var contentNode = document.getElementById('contents');
-var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-var message = continents.map(function (c) {
-  return 'Hello ' + c + '! \n';
-}).join(' ').split("\n");
-var component = React.createElement(
-  'p',
-  null,
-  message
-); // A simple JSX component
-ReactDOM.render(component, contentNode); // Render the component inside the content Node
+
+var IssueFilter = function (_React$Component) {
+    _inherits(IssueFilter, _React$Component);
+
+    function IssueFilter() {
+        _classCallCheck(this, IssueFilter);
+
+        return _possibleConstructorReturn(this, (IssueFilter.__proto__ || Object.getPrototypeOf(IssueFilter)).apply(this, arguments));
+    }
+
+    _createClass(IssueFilter, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                "IssueFilter"
+            );
+        }
+    }]);
+
+    return IssueFilter;
+}(React.Component);
+
+var IssueTable = function (_React$Component2) {
+    _inherits(IssueTable, _React$Component2);
+
+    function IssueTable() {
+        _classCallCheck(this, IssueTable);
+
+        return _possibleConstructorReturn(this, (IssueTable.__proto__ || Object.getPrototypeOf(IssueTable)).apply(this, arguments));
+    }
+
+    _createClass(IssueTable, [{
+        key: "render",
+        value: function render() {
+            var borderedStyle = { border: "1px solid silver", background: "#eee", padding: 10 };
+            return React.createElement(
+                "table",
+                { style: { borderedCollapse: "collapse" } },
+                React.createElement(
+                    "thead",
+                    null,
+                    React.createElement(
+                        "tr",
+                        null,
+                        React.createElement(
+                            "th",
+                            { style: borderedStyle },
+                            "Id"
+                        ),
+                        React.createElement(
+                            "th",
+                            { style: borderedStyle },
+                            "Title"
+                        )
+                    )
+                ),
+                React.createElement(
+                    "tbody",
+                    null,
+                    React.createElement(IssueRow, { issue_id: 1,
+                        issue_title: "Error in Console when clicking Add" }),
+                    React.createElement(IssueRow, { issue_id: 2,
+                        issue_title: "Missing Panel on bottom" }),
+                    React.createElement(IssueRow, { issue_id: 3,
+                        issue_title: "Misaligned columns" })
+                )
+            );
+        }
+    }]);
+
+    return IssueTable;
+}(React.Component);
+
+var IssueRow = function (_React$Component3) {
+    _inherits(IssueRow, _React$Component3);
+
+    function IssueRow() {
+        _classCallCheck(this, IssueRow);
+
+        return _possibleConstructorReturn(this, (IssueRow.__proto__ || Object.getPrototypeOf(IssueRow)).apply(this, arguments));
+    }
+
+    _createClass(IssueRow, [{
+        key: "render",
+        value: function render() {
+            var borderedStyle = { border: "1px solid silver", padding: 8 };
+            return React.createElement(
+                "tr",
+                null,
+                React.createElement(
+                    "td",
+                    { style: borderedStyle },
+                    this.props.issue_id
+                ),
+                React.createElement(
+                    "td",
+                    { style: borderedStyle },
+                    this.props.issue_title
+                )
+            );
+        }
+    }]);
+
+    return IssueRow;
+}(React.Component);
+
+var IssueAdd = function (_React$Component4) {
+    _inherits(IssueAdd, _React$Component4);
+
+    function IssueAdd() {
+        _classCallCheck(this, IssueAdd);
+
+        return _possibleConstructorReturn(this, (IssueAdd.__proto__ || Object.getPrototypeOf(IssueAdd)).apply(this, arguments));
+    }
+
+    _createClass(IssueAdd, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                "IssueAdd"
+            );
+        }
+    }]);
+
+    return IssueAdd;
+}(React.Component);
+
+var IssueList = function (_React$Component5) {
+    _inherits(IssueList, _React$Component5);
+
+    function IssueList() {
+        _classCallCheck(this, IssueList);
+
+        return _possibleConstructorReturn(this, (IssueList.__proto__ || Object.getPrototypeOf(IssueList)).apply(this, arguments));
+    }
+
+    _createClass(IssueList, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "h1",
+                    null,
+                    "IssueTracker"
+                ),
+                React.createElement(IssueFilter, null),
+                React.createElement("hr", null),
+                React.createElement(IssueTable, null),
+                React.createElement("hr", null),
+                React.createElement(IssueAdd, null)
+            );
+        }
+    }]);
+
+    return IssueList;
+}(React.Component);
+
+ReactDOM.render(React.createElement(IssueList, null), contentNode); // Render the component inside the content Node
